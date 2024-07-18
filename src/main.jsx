27d17e73053c77a0603ae447f5bfd46c553f8a34
Fabricio <fuentesfabricio17@gmail.com>
2024-07-18 17:39:10 -0400

    actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
 import { Provider } from 'react-redux'
 import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,11 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <App />
     </Provider>
-=======
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
->>>>>>> 63110bac7face11e77afe515cfcda91156dcdd13
   </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: "/shoppingreact",
-=======
-    base: "/IBM_React_Lab",
->>>>>>> 63110bac7face11e77afe515cfcda91156dcdd13
   plugins: [react()],
 })
